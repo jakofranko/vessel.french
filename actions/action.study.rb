b1efdeb441d1892c8f8135f332e387e722f03095
Jake Franklin <jacob.d.franklin@gmail.com>
2019-01-29 22:04:53 -0700

    Fixed bug with uninitialized host
    Attributes cannot be accessed from the host when initializing actions, 
    because the host itself is not fully initialized yet. Technically it 
    will work, but will throw errors.

--- actions/action.study.rb
+++ actions/action.study.rb
@@ -3,86 +3,86 @@
 
 class ActionStudy
 
-  include Action
+    include Action
 
-  def initialize q = nil
+    def initialize q = nil
 
-    super
+        super
 
-    @name = "Study"
-    @docs = "Basic French words and phrases. Use in the console via \"bind french study\""
-    @vocabulary = Memory_Array.new("vocabulary", @host.path).to_a
+        @name = "Study"
+        @docs = "Basic French words and phrases. Use in the console via \"bind french study\""
 
-  end
-
-  def act q = nil
-
-    if !$nataniev.console_memory[@host.name] then
-        $nataniev.console_memory[@host.name] = {"active" => nil, "history" => []}
     end
 
-    active_word = $nataniev.console_memory[@host.name]['active']
+    def act q = nil
+        if !@vocabulary then @vocabulary = Memory_Array.new("vocabulary", @host.path).to_a end
 
-    if q.to_s != "" && active_word
-      if active_word.english.like(q)
-        active_word.score = 1
-        puts "Correct!"
-        puts score
-      else
-        active_word.score = -1
-        puts "Sorry! The english translation of #{active_word.french} is \"#{active_word.english}\"."
-        puts score
-      end
-    elsif active_word
-      active_word.score = 0
-      puts "The English translation of #{active_word.french.capitalize} is \"#{active_word.english}\"."
+        if !$nataniev.console_memory[@host.name] then
+            $nataniev.console_memory[@host.name] = {"active" => nil, "history" => []}
+        end
+
+        active_word = $nataniev.console_memory[@host.name]['active']
+
+        if q.to_s != "" && active_word
+            if active_word.english.like(q)
+                active_word.score = 1
+                puts "Correct!"
+                puts score
+            else
+                active_word.score = -1
+                puts "Sorry! The english translation of #{active_word.french} is \"#{active_word.english}\"."
+                puts score
+            end
+        elsif active_word
+            active_word.score = 0
+            puts "The English translation of #{active_word.french.capitalize} is \"#{active_word.english}\"."
+        end
+
+        word = @vocabulary.sample
+        active_word = Word.new(word['ENGLISH'], word['FRENCH'])
+        $nataniev.console_memory[@host.name]['active'] = active_word
+        $nataniev.console_memory[@host.name]['history'].push(active_word)
+
+        return "##{history.length}: What is #{active_word.french} in english?"
+
     end
 
-    word = @vocabulary.sample
-    active_word = Word.new(word['ENGLISH'], word['FRENCH'])
-    $nataniev.console_memory[@host.name]['active'] = active_word
-    $nataniev.console_memory[@host.name]['history'].push(active_word)
+    def score
 
-    return "##{history.length}: What is #{active_word.french} in english?"
+        known = {}
+        points = 0
+        history.each do |word|
+            known[word.french] = word.english
+            points += word.score
+        end
 
-  end
+        progress = ((known.length/@vocabulary.length.to_f) * 1000).to_i
+        success  = ((points/known.length.to_f) * 1000).to_i
 
-  def score
+        return "Your score is #{points}pts, Success #{success.to_f/10}%, Progress #{progress.to_f/10}%"
 
-    known = {}
-    points = 0
-    history.each do |word|
-      known[word.french] = word.english
-      points += word.score
     end
 
-    progress = ((known.length/@vocabulary.length.to_f) * 1000).to_i
-    success  = ((points/known.length.to_f) * 1000).to_i
+    def history
 
-    return "Your score is #{points}pts, Success #{success.to_f/10}%, Progress #{progress.to_f/10}%"
+        return $nataniev.console_memory[@host.name]['history']
 
-  end
-
-  def history
-
-    return $nataniev.console_memory[@host.name]['history']
-
-  end
+    end
 
 end
 
 class Word
 
-  attr_accessor :english
-  attr_accessor :french
-  attr_accessor :score
+    attr_accessor :english
+    attr_accessor :french
+    attr_accessor :score
 
-  def initialize english, french
+    def initialize english, french
 
-    @english = english
-    @french = french
-    @score   = 0
+        @english = english
+        @french = french
+        @score   = 0
 
-  end
+    end
 
 end
